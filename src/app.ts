cdd1bbeaef14fb146f9b3a65a55073fbf78bb0d9
Robinhood <okaforchidubem7@gmail.com>
2024-11-27 14:44:36 -0800

    removed unnecessary comments

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,6 @@ import connectToMongodb from "./config/mongodbConfig"
 
 connectToMongodb()
 
-
-// dotenv.config();
-
-// const SEPOLIA_TESTNET_URL = process.env.SEPOLIA_TESTNET_URL;
-
-// const provider = new ethers.JsonRpcProvider(SEPOLIA_TESTNET_URL);
-// const fundingContractAddress = "0x5091418cF113CeDBDbea8E85d2161b96cDA5f85d"; // Deployed contract address
-// const FundingContractABI = abi;
-// const fundingContract = new ethers.Contract(fundingContractAddress, FundingContractABI, provider);
-
-
-
-
-
 const server = new ApolloServer({
     typeDefs,
     resolvers
--- src/utils/isAddressValid.ts
+++ src/utils/isAddressValid.ts
@@ -1,6 +1,6 @@
 const isValidEthereumAddress = (address: string): boolean => {
-    const regex = /^0x[a-fA-F0-9]{40}$/;
-    return regex.test(address);
-};
+    const regex = /^0x[a-fA-F0-9]{40}$/
+    return regex.test(address)
+}
 
 export default isValidEthereumAddress
